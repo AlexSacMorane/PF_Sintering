476ced8dfd9988379c2440a071e0a7b9a4cb52a9
AlexSacMorane <al.sacmorane@gmail.com>
2025-08-06 10:50:51 +0200

    make the main script as callable

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import os, shutil, time
 # Own 
 from CreateIC import *
 from WriteI import *
+from SortFiles import *
 
 #-------------------------------------------------------------------------------
 # Functions
@@ -37,113 +38,129 @@ def index_to_str(j):
         j_str = str(j)
     return j_str
 
+#-------------------------------------------------------------------------------
+# MAiN code
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__':
 
 #-------------------------------------------------------------------------------
 # Parameter
 #-------------------------------------------------------------------------------
 
-# domain 
-x_min = -30
-x_max =  30
-y_min = -30
-y_max =  30
-n_margins = 6 # number of pixels as margins on the size
+    # domain 
+    x_min = -30
+    x_max =  30
+    y_min = -30
+    y_max =  30
+    n_margins = 6 # number of pixels as margins on the size
 
-# mesh
-n_mesh = 300 # number of element in x direction of the mesh
-             # the number of nodes is n_mesh+1
-d_mesh = min(x_max-x_min, y_max-y_min)/n_mesh # size of the mesh element
+    # mesh
+    n_mesh = 300 # number of element in x direction of the mesh
+                # the number of nodes is n_mesh+1
+    d_mesh = min(x_max-x_min, y_max-y_min)/n_mesh # size of the mesh element
 
-# description of the PSD
-n_grains = 9
-mean_R = 15/2
-var_R = 0.2 # -
+    # description of the PSD
+    n_grains = 9
+    mean_R = 15/2
+    var_R = 0.2 # -
 
-# parameters for IC
-n_steps = 30 # steeply increase the radius
+    # parameters for IC
+    n_steps = 30 # steeply increase the radius
 
-# Description of the phase field variables
-A_free_energy = 16 # the energy barrier value used for free energies description
-B_free_energy = 1 # the energy barrier value used for free energies description
-n_int = 6 # number of mesh in the interface
-w_int = d_mesh*n_int # the interface thickness
-kappa_eta = 0.5 # gradient coefficient for free energies (eta)
-kappa_c = 1 # gradient coefficient for free energies (c)
-L = 1 # Mobility value used for free energies (eta)
-M = L # Mobility value used for free energies (c)
+    # Description of the phase field variables
+    A_free_energy = 16 # the energy barrier value used for free energies description
+    B_free_energy = 1 # the energy barrier value used for free energies description
+    n_int = 6 # number of mesh in the interface
+    w_int = d_mesh*n_int # the interface thickness
+    kappa_eta = 0.5 # gradient coefficient for free energies (eta)
+    kappa_c = 1 # gradient coefficient for free energies (c)
+    L = 1 # Mobility value used for free energies (eta)
+    M = L # Mobility value used for free energies (c)
 
-# computing information
-n_proc = 4 # number of processor used
-crit_res = 1e-4 # convergence criteria on residual
+    # PF time parameters
+    dt_PF = 0.01  # time step
+    n_ite_max = 1 # maximum number of iteration
 
-# PF time parameters
-dt_PF = 0.01  # time step
-n_ite_max = 1 # maximum number of iteration
+    # computing information
+    n_proc = 4 # number of processor used
+    crit_res = 1e-4 # convergence criteria on residual
 
-# compute performances
-tic = time.perf_counter()
+    # sorting files
+    reduce_vtk = True # reduce or not the number of vtk
+    n_vtk_max = 30 # if reduced, maximal number of vtk files
+
+    # compute performances
+    tic = time.perf_counter()
 
 #------------------------------------------------------------------------------
 # create dict
 #------------------------------------------------------------------------------
 
-dict_user = {
-    'x_min': x_min,
-    'x_max': x_max,
-    'y_min': y_min,
-    'y_max': y_max,
-    'n_margins': n_margins,
-    'd_mesh': d_mesh,
-    'n_grains': n_grains,
-    'mean_R': mean_R,
-    'var_R': var_R,
-    'n_steps': n_steps,
-    'w_int': w_int,
-    'L': L,
-    'kappa_eta': kappa_eta,
-    'M': M,
-    'kappa_c': kappa_c,
-    'A': A_free_energy,
-    'B': B_free_energy,
-    'n_proc': n_proc,
-    'crit_res': crit_res,
-    'n_ite_max': n_ite_max,
-    'dt_PF': dt_PF
-}
+    dict_user = {
+        'x_min': x_min,
+        'x_max': x_max,
+        'y_min': y_min,
+        'y_max': y_max,
+        'n_margins': n_margins,
+        'd_mesh': d_mesh,
+        'n_grains': n_grains,
+        'mean_R': mean_R,
+        'var_R': var_R,
+        'n_steps': n_steps,
+        'w_int': w_int,
+        'L': L,
+        'kappa_eta': kappa_eta,
+        'M': M,
+        'kappa_c': kappa_c,
+        'A': A_free_energy,
+        'B': B_free_energy,
+        'n_proc': n_proc,
+        'crit_res': crit_res,
+        'n_ite_max': n_ite_max,
+        'dt_PF': dt_PF,
+        'reduce_vtk': reduce_vtk,
+        'n_vtk_max': n_vtk_max,
+    }
 
 #-------------------------------------------------------------------------------
 # Plan simulation
 #-------------------------------------------------------------------------------
 
-create_folder('output')
-create_folder('data')
+    create_folder('output')
+    create_folder('output/vtk')
+    create_folder('data')
 
 #-------------------------------------------------------------------------------
 # Generation and save of the microstructure
 #-------------------------------------------------------------------------------
 
-generate_microstructure(dict_user)
+    generate_microstructure(dict_user)
 
 #-------------------------------------------------------------------------------
 # write the input file
 #-------------------------------------------------------------------------------
 
-write_input_pf(dict_user)
+    write_input_pf(dict_user)
 
 #-------------------------------------------------------------------------------
 # run the simulation
 #-------------------------------------------------------------------------------
 
-print('\n run simulation')
-os.system('mpiexec -n '+str(dict_user['n_proc'])+' ~/projects/moose/modules/phase_field/phase_field-opt -i PF_Sintering.i')
-
-
-raise ValueError('stop')
+    print('\n run simulation')
+    os.system('mpiexec -n '+str(dict_user['n_proc'])+' ~/projects/moose/modules/phase_field/phase_field-opt -i PF_Sintering.i')
 
 #-------------------------------------------------------------------------------
 # sort files
 #-------------------------------------------------------------------------------
 
-os.rename('PF_Sintering_csv.csv','output/PF_Sintering_csv.csv')
-os.rename('PF_Sintering_out.e','output/PF_Sintering_out.e')
-os.rename('PF_Sintering.i','output/PF_Sintering.i')
+    # move
+    os.rename('PF_Sintering_csv.csv','output/PF_Sintering_csv.csv')
+    os.rename('PF_Sintering_out.e','output/PF_Sintering_out.e')
+    os.rename('PF_Sintering.i','output/PF_Sintering.i')
+    Sort_vtk(dict_user)
+    # remove
+    shutil.rmtree('data')
+
+
+    raise ValueError('stop')
