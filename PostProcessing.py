04edb2b7d3fe0acf571ba54a257a43f89038c5ca
AlexSacMorane <al.sacmorane@gmail.com>
2025-08-06 15:42:38 +0200

    add morphometers

--- PostProcessing.py
+++ PostProcessing.py
@@ -5,7 +5,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pyvista as pv
-import pickle
+import pickle, porespy, skimage
 
 # Own
 from main import index_to_str
@@ -26,6 +26,11 @@ def pp(dict_user):
     dict_tempo = {
         'L_M_Neck': [],
         'L_freeSurface': [],
+        'L_M0': [],
+        'L_M1': [],
+        'L_M2': [],
+        'L_M3': [],
+        'map': None
     }
     # iterate on the .pvtu files
     for i in range(dict_user['last_j']+1):
@@ -33,15 +38,24 @@ def pp(dict_user):
         # read the .pvtu
         ReadPVTU(dict_user, dict_tempo, 'output/vtk/PF_Sintering_other_'+index_to_str(i)+'.pvtu')
 
+        # Biswas, 2018
         # compute the neck area
         ComputeNeck(dict_tempo)
         # compute the free surface area
         ComputeFreeSurface(dict_tempo)
+    
+        # Guevel, 2022
+        RebuildMap(dict_user, dict_tempo)
+        ComputeMorphometers(dict_tempo)
 
+    # Biswas, 2018
     # plot the neck area
     PlotNeck(dict_tempo)
     # plot the free surface area
     PlotFreeSurface(dict_tempo)
+
+    # Guevel, 2022
+    PlotMorphometers(dict_tempo)
 
 #-------------------------------------------------------------------------------
 
@@ -202,6 +216,112 @@ def PlotFreeSurface(dict_tempo):
     ax1.tick_params(axis='both', labelsize=20, width=3, length=3)
     fig.tight_layout()
     fig.savefig('output/evol_ite_freesurface.png')
+    plt.close(fig)
+
+#-------------------------------------------------------------------------------
+
+def RebuildMap(dict_user, dict_tempo):
+    '''
+    Rebuild the map from the output of the pyvista.
+    '''
+    # initialization
+    M_void = np.zeros((len(dict_user['L_y']), len(dict_user['L_x'])))
+    M_solid = np.zeros((len(dict_user['L_y']), len(dict_user['L_x'])))
+
+    # the map is not know
+    if dict_tempo['map'] == None:
+        # init map
+        map = []
+        # iterate on the points
+        for i_point in range(len(dict_tempo['L_points'])):
+            # search node in the mesh
+            L_search = list(abs(np.array(dict_user['L_x']-list(dict_tempo['L_points'][i_point])[0])))
+            i_x = L_search.index(min(L_search))
+            L_search = list(abs(np.array(dict_user['L_y']-list(dict_tempo['L_points'][i_point])[1])))
+            i_y = L_search.index(min(L_search))
+            # save map
+            map.append([i_x, i_y])
+            # rebuild maps
+            if dict_tempo['L_c'][i_point] < 0.5:
+                M_void[-1-i_y, i_x] = 1
+            else:
+                M_solid[-1-i_y, i_x] = 1
+        # save map
+        dict_tempo['map'] = map
+
+    # the map is know
+    else:
+        # iterate on the points
+        for i_point in range(len(dict_tempo['L_points'])):
+            # read the map
+            i_x = dict_tempo['map'][i_point][0]
+            i_y = dict_tempo['map'][i_point][1]
+            # rebuild maps
+            if dict_tempo['L_c'][i_point] < 0.5:
+                M_void[-1-i_y, i_x] = 1
+            else:
+                M_solid[-1-i_y, i_x] = 1
+
+    # save
+    dict_tempo['M_void'] = M_void.copy()
+    dict_tempo['M_solid'] = M_solid.copy()
+
+#-------------------------------------------------------------------------------
+
+def ComputeMorphometers(dict_tempo):
+    '''
+    Compute the morphometers.
+
+    see Guével, 2022
+    '''
+    # M0 porosity
+    M0 = porespy.metrics.porosity(dict_tempo['M_void'])
+    # M1 perimeter
+    M1 = skimage.measure.perimeter(dict_tempo['M_solid'])
+    # M2 grain size
+    M2 = 0
+    # M3 Euler
+    M3 = skimage.measure.euler_number(dict_tempo['M_solid'])
+
+    # save
+    dict_tempo['L_M0'].append(M0)
+    dict_tempo['L_M1'].append(M1)
+    dict_tempo['L_M2'].append(M2)
+    dict_tempo['L_M3'].append(M3)
+
+#-------------------------------------------------------------------------------
+
+def PlotMorphometers(dict_tempo):
+    '''
+    Plot the morphometers.
+
+    see Guevel, 2022.
+    '''
+    # open figure
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2,figsize=(2*16,2*9))
+    # M0
+    ax1.plot(dict_tempo['L_M0'], linewidth=6)
+    ax1.set_xlabel('iteration (-)', fontsize=25)
+    ax1.set_ylabel('M0 porosity (-)', fontsize=25)
+    ax1.tick_params(axis='both', labelsize=20, width=3, length=3)
+    # M1
+    ax2.plot(dict_tempo['L_M1'], linewidth=6)
+    ax2.set_xlabel('iteration (-)', fontsize=25)
+    ax2.set_ylabel('M1 perimeter (-)', fontsize=25)
+    ax2.tick_params(axis='both', labelsize=20, width=3, length=3)
+    # M2
+    ax3.plot(dict_tempo['L_M2'], linewidth=6)
+    ax3.set_xlabel('iteration (-)', fontsize=25)
+    ax3.set_ylabel('M2 grain size (-)', fontsize=25)
+    ax3.tick_params(axis='both', labelsize=20, width=3, length=3)
+    # M3
+    ax4.plot(dict_tempo['L_M3'], linewidth=6)
+    ax4.set_xlabel('iteration (-)', fontsize=25)
+    ax4.set_ylabel('M3 euler (-)', fontsize=25)
+    ax4.tick_params(axis='both', labelsize=20, width=3, length=3)
+    # close
+    fig.tight_layout()
+    fig.savefig('output/evol_ite_morphometers.png')
     plt.close(fig)
 
 #-------------------------------------------------------------------------------
